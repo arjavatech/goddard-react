f7f10f6dbbc4bb2f7505be507be914b8ad35a123
Mani-Arjava <mani.arjava@gmail.com>
2025-08-23 16:36:31 +0530

    API endpoint changes

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,4 +1,5 @@
 export const school_id = 1;
-export const api_base_url = "http://localhost:8000";
-// 'https://hfj4ckons6.execute-api.ap-south-1.amazonaws.com/dev';
+export const api_base_url = 'https://hfj4ckons6.execute-api.ap-south-1.amazonaws.com/dev';
+// "http://localhost:8000";
+
 export const updated_by = "Admin";
